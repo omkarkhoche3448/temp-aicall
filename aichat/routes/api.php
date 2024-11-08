5b454926e6d0eefcd7f7722ffa4cd2fb5feb8cb5
SohamMhatre09 <ichbinsoham@gmail.com>
2024-11-08 07:52:14 +0530

    clean up

--- aichat/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Http\Controllers\ChatController;
-
-Route::post('/chat/send', [ChatController::class, 'sendMessage']);
-Route::get('/chat/messages', [ChatController::class, 'getMessages']);
--- aichat/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\ChatController;
-
-Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
-Route::post('/chat/send', [ChatController::class, 'send'])->name('chat.send');
-Route::get('/chat/messages', [ChatController::class, 'getMessages'])->name('chat.messages');
